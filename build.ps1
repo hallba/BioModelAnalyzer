3628942b2fc98397f6fd460228e1254f02d847dc
Rachel-Alcraft <r.alcraft@ucl.ac.uk>
2022-08-18 16:16:34 +0100

    CLean ps1 to remove obj directories

--- build.ps1
+++ build.ps1
@@ -1,5 +1,10 @@
 # Copyright (c) Microsoft Research 2016
 # License: MIT. See LICENSE
+
+
+# First delete obj files that are causing conflict from multiple build environments
+.\Clean.ps1
+
 if ([Environment]::Is64BitOperatingSystem) {
     $pfiles = ${env:PROGRAMFILES(X86)}
     $platform = '/p:Platform="x64"'
